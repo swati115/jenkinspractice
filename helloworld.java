9ff98c563531fecb5decece06b1a26be0cb239d3
swati115 <55168924+swati115@users.noreply.github.com>
2019-09-10 22:09:36 -0400

    committed helloworld

--- helloworld.java
+++ helloworld.java
@@ -3,4 +3,8 @@ Class Hello{
   public void helloWorld(){
   System.out.println("Hello World!!!");
   }
+  public static void main(String[] args){
+    Hello hello = new Hello();
+    hello.helloWorld();
+  }
 }
