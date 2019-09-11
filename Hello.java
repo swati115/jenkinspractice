288bf3cc860adb7bcbe39a71e223c86878d041e6
swati115 <55168924+swati115@users.noreply.github.com>
2019-09-10 22:28:48 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 class Hello{
 
   public void helloWorld(){
-  System.out.println("Hello World!!!");
+  System.out.println("Hello World!!!I am changed..jenkins job shud run now automatically");
   }
   public static void main(String[] args){
     Hello hello = new Hello();
